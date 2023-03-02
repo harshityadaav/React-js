04584c2d3286fccf410d54c23fc15a7c3615ea1e
harshityadaav <105654252+harshityadaav@users.noreply.github.com>
2023-03-03 00:34:03 +0530

    first commit

---
+++ my-app/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Expense Item</h2>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
